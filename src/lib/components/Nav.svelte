85702a48e6b870e3460ad4f95aa6b84b3f0d1ca1
Stephen Wilde <74353707+kinkwilde@users.noreply.github.com>
2024-12-25 02:24:04 +0000

    Test

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,8 +1,8 @@
 <nav class="border-y border-black bg-gray-900 text-white">
 	<div class="container py-2">
 		<ul class="flex list-none justify-center space-x-4 [&_a]:uppercase [&_a]:text-white">
-			<li><a href="{base}/">Home</a></li>
-			<li><a href="{base}/loot">Loot Table</a></li>
+			<li><a href="{globalThis.$base}/">Home</a></li>
+			<li><a href="{globalThis.$base}/loot">Loot Table</a></li>
 			<li><a href="https://discord.gg/k9fRuhe5kw" target="_blank">Discord</a></li>
 		</ul>
 	</div>
--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,12 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'docs',
 			assets: 'docs',
 			fallback: null
 		}),
 		paths: {
-			// Use the repository name as the base path
 			base: '/berkshireblackoutchamber'
 		}
 	}
