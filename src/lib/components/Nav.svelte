cf4ad611600bc32ddcbacfe673cd77ce88213feb
Stephen Wilde <74353707+kinkwilde@users.noreply.github.com>
2024-12-25 02:14:04 +0000

    Fix

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,7 +2,7 @@
 	<div class="container py-2">
 		<ul class="flex list-none justify-center space-x-4 [&_a]:uppercase [&_a]:text-white">
 			<li><a href="/">Home</a></li>
-			<li><a href="/loot/">Loot Table</a></li>
+			<li><a href="/loot">Loot Table</a></li>
 			<li><a href="https://discord.gg/k9fRuhe5kw" target="_blank">Discord</a></li>
 		</ul>
 	</div>
